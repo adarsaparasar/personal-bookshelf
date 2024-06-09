e363b649a96c744db32dc16fff9101baa875927d
adarsaparasar <adarsaparasarbehera@gmail.com>
2024-06-09 13:50:54 +0530

    Bookshelf
    Added Button

--- src/pages/BookshelfPage.js
+++ src/pages/BookshelfPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import { Link } from 'react-router-dom';
 
 const BookshelfPage = () => {
   const [bookshelf, setBookshelf] = useState([]);
@@ -10,18 +10,21 @@ const BookshelfPage = () => {
   }, []);
 
   return (
-      <div>
-          <a href="/" >Back to Search</a>
-      <h1>My Bookshelf</h1>
+    <div>
+      <div className="bookshelf-header">
+        <h1>My Bookshelf</h1>
+        <Link to="/">
+          <button>Back to Search</button>
+        </Link>
+      </div>
       <div className="bookshelf">
-        {bookshelf.map(book => (
-          <div key={book.key} className="book-card">
+        {bookshelf.map((book, index) => (
+          <div className="book-card" key={index}>
             <h3>Book Title: {book.title}</h3>
             <p>Edition Count: {book.edition_count}</p>
           </div>
         ))}
-          </div>
-          
+      </div>
     </div>
   );
 };
